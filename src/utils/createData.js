69ad70092d1c50ecfe424abab92f5c61a41c8445
davidhellmann <david@hellmann.io>
2022-08-17 22:00:55 +0200

    🐛 fix: merge objects in the right order

--- src/utils/createData.js
+++ src/utils/createData.js
@@ -4,11 +4,11 @@ module.exports = (options, data) => {
     if (defaults.settings && defaults.values && options.settings && options.values) {
         // Add settings
         data.settings = Object.fromEntries(Object.entries({
-            ...options.settings, ...defaults.settings
+            ...defaults.settings, ...options.settings
         }).filter(([key]) => key !== 'unit').filter(([key]) => key !== 'prefix'));
 
         // Add values
-        data.values = {...options.values, ...defaults.values}
+        data.values = {...defaults.values, ...options.values }
 
         // Add prefix
         data.prefix = options.settings?.prefix || defaults.settings?.prefix || ''
