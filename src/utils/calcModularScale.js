39d5634d6ac518e57e209990972c50f825f7d011
David Hellmann <david.hellmann@dynatrace.com>
2022-08-18 08:55:26 +0200

    🐛 fix: remove double check for defaults / respect unit settings

--- src/utils/calcModularScale.js
+++ src/utils/calcModularScale.js
@@ -1,14 +1,14 @@
 const defaults = require("../config/defaults");
 
 module.exports = (value, data) => {
-    if (data.settingsAreNumbers && data.settings && defaults.settings) {
-        const sFtMin = data.settings?.fontSizeMin || defaults.settings?.fontSizeMin
-        const sFtMax = data.settings?.fontSizeMax || defaults.settings?.fontSizeMax
-        const sFtRMin = data.settings?.ratioMin || defaults.settings?.ratioMin
-        const sFtRMax = data.settings?.ratioMax || defaults.settings?.ratioMax
-        const sFtSMin = data.settings?.screenMin || defaults.settings?.screenMin
-        const sFtSMax = data.settings?.screenMax || defaults.settings?.screenMax
-        const unit = data.settings?.unit || defaults.settings?.unit
+    if (data.settingsAreNumbers && data.settings) {
+        const sFtMin = data.settings?.fontSizeMin
+        const sFtMax = data.settings?.fontSizeMax
+        const sFtRMin = data.settings?.ratioMin
+        const sFtRMax = data.settings?.ratioMax
+        const sFtSMin = data.settings?.screenMin
+        const sFtSMax = data.settings?.screenMax
+        const unit = data.unit
         const sFtUnit = typeof unit === 'string' ? unit : 'rem';
         const ftMin = sFtMin * Math.pow(sFtRMin, value);
         const ftMax = sFtMax * Math.pow(sFtRMax, value);
